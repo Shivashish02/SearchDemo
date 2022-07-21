28550f173954e81146a981fad028138000f85d6d
Shivashish <shivashishsinghrawat02@gmail.com>
2022-07-22 05:00:36 +0530

    view product and like

--- src/components/cardlarge.js
+++ src/components/cardlarge.js
@@ -1,9 +1,17 @@
 import Rating from "./rating";
-
+import heart from "../imgs/heart.png";
+import { useState } from "react";
 export default function Cardlarge(props) {
+    const [display, setDisplay] = useState({ display: "none" });
     return (
-        <div className="cardlarge">
-            <div className="imagecontainer"><img id="imglarge" src={props.image} /></div>
+        <div className="cardlarge" onMouseEnter={() => setDisplay({ display: "block" })}
+            onMouseLeave={() => setDisplay({ display: "none" })}>
+            <div className="imagecontainer">
+                <img id="imglarge" src={props.image} />
+                <img id="icon" src={heart} />
+                <div id="viewprod" style={display}></div>
+                <div id="viewprodtext" style={display}>View Product</div>
+            </div>
             <div>
                 <p id="prod">{props.product}</p>
                 <div className="sale">
